ed4dd54b4a0fbee30b0a70fac9b3083a42389ecf
nhn2000 <namnguyen892000@gmail.com>
2020-07-09 09:08:54 +0700

    fix code

--- dataset/modelnetdataset.py
+++ dataset/modelnetdataset.py
@@ -2,6 +2,7 @@ import numpy as np
 import warnings
 import os
 from torch.utils.data import Dataset
+from tqdm import tqdm
 
 warnings.filterwarnings('ignore')
 
@@ -62,14 +63,14 @@ class ModelNetDataLoader(Dataset):
 
         self.cache_size = cache_size  # how many data points to cache in memory
         self.cache = {}  # from index to (point_set, cls) tuple
+        self.data_set = self.get_data()
 
     def __len__(self):
         return len(self.datapath)
 
-    def _get_item(self, index):
-        if index in self.cache:
-            point_set, cls = self.cache[index]
-        else:
+    def get_data(self):
+        data_set = list()
+        for index in tqdm(range(len(self.datapath))):
             fn = self.datapath[index]
             cls = self.classes[self.datapath[index][0]]
             cls = np.array([cls]).astype(np.int32)
@@ -77,20 +78,42 @@ class ModelNetDataLoader(Dataset):
             if self.uniform:
                 point_set = farthest_point_sample(point_set, self.npoints)
             else:
-                point_set = point_set[0:self.npoints, :]
+                point_set = point_set[0: self.npoints, :]
 
             point_set[:, 0:3] = pc_normalize(point_set[:, 0:3])
 
             if not self.normal_channel:
                 point_set = point_set[:, 0:3]
 
-            if len(self.cache) < self.cache_size:
-                self.cache[index] = (point_set, cls)
+            data_set.append((point_set, cls))
 
-        return point_set, cls
+        return data_set
+
+    # def _get_item(self, index):
+    #     if index in self.cache:
+    #         point_set, cls = self.cache[index]
+    #     else:
+    #         fn = self.datapath[index]
+    #         cls = self.classes[self.datapath[index][0]]
+    #         cls = np.array([cls]).astype(np.int32)
+    #         point_set = np.loadtxt(fn[1], delimiter=',').astype(np.float32)
+    #         if self.uniform:
+    #             point_set = farthest_point_sample(point_set, self.npoints)
+    #         else:
+    #             point_set = point_set[0:self.npoints, :]
+    #
+    #         point_set[:, 0:3] = pc_normalize(point_set[:, 0:3])
+    #
+    #         if not self.normal_channel:
+    #             point_set = point_set[:, 0:3]
+    #
+    #         if len(self.cache) < self.cache_size:
+    #             self.cache[index] = (point_set, cls)
+    #
+    #     return point_set, cls
 
     def __getitem__(self, index):
-        return self._get_item(index)
+        return self.data_set[index]
 
 
 if __name__ == '__main__':
