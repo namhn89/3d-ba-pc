5d42d111fef0356361546625ef4fef99c78b1796
nhn2000 <namnguyen892000@gmail.com>
2020-08-27 09:29:35 +0700

    upload files

--- models/dgcnn_cls.py
+++ models/dgcnn_cls.py
@@ -150,7 +150,7 @@ class get_loss(nn.Module):
     def __init__(self):
         super(get_loss, self).__init__()
 
-    def forward(self, pred, target, trans_feat,  smoothing=True):
+    def forward(self, pred, target, trans_feat, smoothing=True):
 
         target = target.contiguous().view(-1)
         target = target.int()
