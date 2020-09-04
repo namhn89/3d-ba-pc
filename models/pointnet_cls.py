7e043eddd7894715dbf0e0c28150e1625b506a64
nhn2000 <namnguyen892000@gmail.com>
2020-09-04 19:24:42 +0700

    upload files

--- models/pointnet_cls.py
+++ models/pointnet_cls.py
@@ -14,16 +14,17 @@ class get_model(nn.Module):
             channel = 3
         self.feat = PointNetEncoder(global_feat=True, feature_transform=True, channel=channel)
         self.fc1 = nn.Linear(1024, 512)
+        self.dropout = nn.Dropout(p=0.4)
         self.fc2 = nn.Linear(512, 256)
         self.fc3 = nn.Linear(256, k)
-        self.dropout = nn.Dropout(p=0.4)
+        self.dropout1 = nn.Dropout(p=0.4)
         self.bn1 = nn.BatchNorm1d(512)
         self.bn2 = nn.BatchNorm1d(256)
         self.relu = nn.ReLU()
 
     def forward(self, x):
         x, trans, trans_feat, hx, max_pool = self.feat(x)
-        x = F.relu(self.bn1(self.fc1(x)))
+        x = F.relu(self.bn1(self.dropout1(self.fc1(x))))
         x = F.relu(self.bn2(self.dropout(self.fc2(x))))
         x = self.fc3(x)
         x = F.log_softmax(x, dim=1)
