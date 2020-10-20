899e0a89f56cad59c298d1dc792dac687c495dae
nhn2000 <namnguyen892000@gmail.com>
2020-10-20 16:20:04 +0700

    upload files

--- runs/central_point.sh
+++ runs/central_point.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python ba_attack.py --random --mode_attack central_point --model pointnet_cls
-python ba_attack.py --random --mode_attack central_point --model pointnet_cls --dataset scanobjectnn_pb_t50_rs
+python ba_attack.py --random --attack_method central_point --model pointnet_cls
+python ba_attack.py --random --attack_method central_point --model pointnet_cls --dataset scanobjectnn_pb_t50_rs --epochs 500
--- runs/pointnet2_2048.sh
+++ runs/pointnet2_2048.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 python train_cls.py --dataset scanobjectnn_pb_t50_rs --num_point 1024 --model pointnet_cls --permanent_point
